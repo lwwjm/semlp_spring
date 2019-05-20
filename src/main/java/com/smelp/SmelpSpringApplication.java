4d1f0a721c009e5fd1ffff39afdd465f9dca4ead
lw <574920956@qq.com>
2019-05-20 14:21:25 +0800

    修改

---
+++ src/main/java/com/smelp/SmelpSpringApplication.java
@@ -0,0 +1,15 @@
+package com.smelp;
+
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmelpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmelpSpringApplication.class, args);
+	}
+
+}
--- src/main/java/com/smelp/controller/HomeController.java
+++ src/main/java/com/smelp/controller/HomeController.java
@@ -1,11 +1,13 @@
-package com.smlep.demo;
+package com.smelp.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
+@RequestMapping("/demo")
 public class HomeController {
 	
-	 @RequestMapping("/")
+	 @GetMapping("/test")
 	    public String home(){
 	        return "Hello, Zbook!";
 	    }
