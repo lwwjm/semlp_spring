3b4084a7350032aaed25a28cfd11359a135be659
lw <574920956@qq.com>
2019-05-20 16:20:18 +0800

    redis

--- src/main/java/com/smelp/controller/HomeController.java
+++ src/main/java/com/smelp/controller/HomeController.java
@@ -6,16 +6,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smelp.mapper.UserDao;
+import com.smelp.utils.RedisUtil;
 @RestController
 @RequestMapping("/demo")
 public class HomeController {
 	@Autowired
 	private  UserDao uerDao;
+	@Autowired
+	private RedisUtil redisUtil;
 	
 	 @GetMapping("/test")
 	    public String home(){
 	        return "Hello, Zbook!";
 	    }
 
+	 @GetMapping("/set")
+	  public boolean set() {
+		 boolean set = redisUtil.set("name", "12312");
+		return set;
+	  }
 	 
+	 @GetMapping("/get")
+	  public String get() {
+		 String object = (String) redisUtil.get("name");
+		return object;
+	  }
 }
--- src/test/java/com/smelp/spring/SmelpSpringApplicationTests.java
+++ src/test/java/com/smelp/spring/SmelpSpringApplicationTests.java
@@ -2,15 +2,27 @@ package com.smelp.spring;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.smelp.mapper.UserDao;
+import com.smelp.pojo.DUser;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SmelpSpringApplicationTests {
+	@Autowired
+	private  UserDao  userDao;
 
 	@Test
 	public void contextLoads() {
+		   DUser userDO = new DUser();
+		  
+	        
+	        userDao.save(userDO);
+		
+		
 	}
 
 }
