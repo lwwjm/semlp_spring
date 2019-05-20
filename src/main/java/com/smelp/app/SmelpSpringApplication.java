4183895e979ee3db8a50bc5ca5cb5d3c5a56bbdb
lw <574920956@qq.com>
2019-05-20 10:45:58 +0800

    案例

---
+++ src/main/java/com/smelp/app/SmelpSpringApplication.java
@@ -0,0 +1,14 @@
+package com.smelp.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmelpSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmelpSpringApplication.class, args);
+	}
+
+}
