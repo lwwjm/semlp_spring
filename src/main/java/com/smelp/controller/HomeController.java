8eaa772c2dd4b79a214ad32a26e7f4cdca20c140
lw <574920956@qq.com>
2019-05-23 10:59:49 +0800

    jpademo

--- src/main/java/com/smelp/controller/HomeController.java
+++ src/main/java/com/smelp/controller/HomeController.java
@@ -1,11 +1,22 @@
 package com.smelp.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smelp.mapper.UserDao;
+import com.smelp.pojo.DUser;
 import com.smelp.utils.RedisUtil;
 @RestController
 @RequestMapping("/demo")
@@ -14,6 +25,7 @@ public class HomeController {
 	private  UserDao uerDao;
 	@Autowired
 	private RedisUtil redisUtil;
+	private List findAll;
 	
 	 @GetMapping("/test")
 	    public String home(){
@@ -30,5 +42,27 @@ public class HomeController {
 	  public String get() {
 		 String object = (String) redisUtil.get("name");
 		return object;
+	  }
+	 
+	 @GetMapping("/getUser")
+	  public List<DUser> getUser(String name) {
+		 DUser  user=new DUser();
+		 user.setName(name);
+		 @SuppressWarnings("rawtypes")
+		Specification querySpeci = new Specification() {
+			@Override
+			public Predicate toPredicate(Root root, CriteriaQuery query, CriteriaBuilder criteriaBuilder) {
+				 List<Predicate> predicates = new ArrayList();
+				 if(!StringUtils.isEmpty(user.getName())) {
+	                    predicates.add(criteriaBuilder
+	                            .equal(root.get("name"), user.getName()));
+	                }
+				 return criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
+			}
+			 
+		 };
+		 
+	List findAll2 = uerDao.findAll(querySpeci);
+	return findAll2;
 	  }
 }
--- src/main/java/com/smelp/mapper/UserDao.java
+++ src/main/java/com/smelp/mapper/UserDao.java
@@ -1,10 +1,11 @@
 package com.smelp.mapper;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.smelp.pojo.DUser;
 @Repository
-public interface UserDao extends JpaRepository<DUser, String> {
+public interface UserDao extends JpaRepository<DUser, Long> ,JpaSpecificationExecutor{
 
 }
